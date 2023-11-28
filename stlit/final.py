9803d5f9bd5717502bf35535d56d504370dbcdc7
maro390 <148246756+maro390@users.noreply.github.com>
2023-11-28 23:21:55 +0300

    final.py

--- stlit/final.py
+++ stlit/final.py
@@ -6,50 +6,9 @@ import streamlit as st
 
 df = pd.read_csv("stlit/maro_cleand.csv")
 st.title('gkc')
-popi= df[df[["Place of Residence"]] != "na" ]
-yui=popi["Place of Residence"].value_counts().sort_values(ascending=False)
-xc=yui.sort_values(ascending=False)
-x=xc.head(7)
-
-
-
-
-
-def delivery(nmo):
-    global values, labels
-   
-    values = []
-    labels = []
-    German = [ahmsd_mphsen("German Language Course"),'German Language Course']
-    Specialized_Programming = [ahmsd_mphsen("Specialized Programming Diploma"),"Specialized Programming Diploma"]
-    Reader = [ahmsd_mphsen("Cultured Reader"),"Cultured Reader"]
-    Mathematics = [ahmsd_mphsen("Mathematics (Matific)"),"Mathematics (Matific)"]
-
-    Fundamentals_of_Programming = [ahmsd_mphsen("Fundamentals of Programming"),"Fundamentals_of_Programming"]
-    German_Explorer = [ahmsd_mphsen("German Explorer"),"German Explorer"]
-
-    Online_School = [ahmsd_mphsen("Online School"),"Online School"]
-    Digital_Marketing_Diploma = [ahmsd_mphsen("Digital Marketing Diploma"),"Digital Marketing Diploma"]
-    Graphics_Diploma = [ahmsd_mphsen("Graphics Diploma"),"Graphics Diploma"]
-    Little_Explorer = [ahmsd_mphsen("Little Explorer"),"Little Explorer"]
-    namy=[German,Specialized_Programming,Reader,Mathematics,Fundamentals_of_Programming,German_Explorer,Online_School,Digital_Marketing_Diploma
-         ,Graphics_Diploma,Little_Explorer]
-    for i in namy:
-
-        if i[0] > nmo:
-        
-            values.append(i[0])
-            labels.append(i[1])
-
-
-
-# ##############################################
-
-
-
-
-
-
+xcc= df[df[["Place of Residence"]] != "na" ]
+xc=xcc["Place of Residence"].value_counts()
+x=xc.nlargest(7)
 
 
 
@@ -62,7 +21,7 @@ def main2():
     plt.title("tha most palces that gkc known from")
     plt.xticks(rotation=70)
     st.pyplot(plt)
-tab1, tab2, tab3 , tab4, tab5 = st.tabs(["part 1", "part 2", "part 3", "part 4", "part 5"])
+tab1, tab2, tab3 , tab4, tab5 = st.tabs(["p1", "p2", "p3", "p4", "p5"])
 
 with tab1:
     st.header("tha most palces that gkc known from")
@@ -73,17 +32,19 @@ with tab1:
 
 
 with tab2:
-    st.header("tha place that enroll in corse")
     choice = st.radio("Select one option:", xc.index)
-    def ahmsd_mphsen(hmasa):
-        x=df[df["Place of Residence"] == choice ]
-        y=x[x["Place of Residence"] != "na" ]
-        return y[f"{hmasa}"].sum()
+
+    x=df[df["Place of Residence"] == choice ]
+    y=x[x["Place of Residence"] != "na" ].iloc[:,10:].sum().nlargest(5)
+        
+
     
-    delivery(0.75)
+    
+    
+ 
     urt2=st.button("click            me")
     if urt2:
-        plt.bar(labels, values)
+        plt.bar(y.index, y.values)
         plt.xlabel('Courses ')  # Add x-axis label
         plt.ylabel('Count ')  # Add y-axis label
         plt.title('Course Enrollment ')  # Add title
@@ -94,14 +55,14 @@ with tab2:
 with tab3:
     st.header("the top 5 courses by month")
     moto=8
-    def ahmsd_mphsen(hmasa):
     
-        y=df[df["month"] == moto ]
-        return y[f"{hmasa}"].sum()
-    delivery(25)
+    
+    y=df[df["month"] == moto ].iloc[:,10:].sum().nlargest(7)
+    
+    
     urt3=st.button("click              me")
     if urt3:
-        plt.bar(labels, values)
+        plt.bar(y.index, y.values)
         plt.xlabel('Courses ')  # Add x-axis label
         plt.ylabel('Count ')  # Add y-axis label
         plt.title('the top 5 courses by month ')  # Add title
@@ -111,14 +72,13 @@ with tab3:
         st.pyplot(plt)
 with tab4:
     st.header("the most interesting courses outside Egypt")
-    def ahmsd_mphsen(hmasa):
-        x=df[df["Country"] != "eygpt" ]
-        y=x[x["Country"] != "na" ]
-        return y[f"{hmasa}"].sum()
-    delivery(1.5)
+
+    x=df[df["Country"] != "eygpt"]
+    y=x[x["Country"] != "na" ].iloc[:,10:].sum().nlargest(7)
+
     ur3=st.button("click               me")
     if ur3:
-        plt.bar(labels, values)
+        plt.bar(y.index, y.values)
         plt.xlabel('Courses ')  # Add x-axis label
         plt.ylabel('Count ')  # Add y-axis label
         plt.title('Course Enrollment ')  # Add title
@@ -140,6 +100,7 @@ with tab5:
 
         # Display the chart
         st.pyplot(plt)
+
 
 st.title('bouns part')
 
@@ -166,14 +127,14 @@ with tab11:
 with tab22:
     st.header("What are the relevant courses for each age")
     hamra=st.radio("choice",the_age.index)
-    def ahmsd_mphsen(hmasa):
-        x=df[df["Academic Year"] == hamra ]
-        y=x[x["Academic Year"] != "na" ]
-        return y[f"{hmasa}"].sum()
-    delivery(.5)
+
+    x=df[df["Academic Year"] == hamra ]
+    y=x[x["Academic Year"] != "na" ].iloc[:,10:].sum().nlargest(7)
+
+
     ur3=st.button("click                    me")
     if ur3:
-        plt.bar(labels, values)
+        plt.bar(y.index, y.values)
         plt.xlabel('Courses ')  # Add x-axis label
         plt.ylabel('Count ')  # Add y-axis label
         plt.title('Course Enrollment ')  # Add title
@@ -202,14 +163,13 @@ with tab33:
 with tab44:
     st.header("The site where people come the most from")
     hamraa=st.radio("choice",the_sitee.index)
-    def ahmsd_mphsen(hmasa):
-        x=df[df["Learned About Genius From"] == hamraa ]
-        y=x[x["Learned About Genius From"] != "na" ]
-        return y[f"{hmasa}"].sum()
-    delivery(.5)
+
+    x=df[df["Learned About Genius From"] == hamraa ]
+    y=x[x["Learned About Genius From"] != "na" ].iloc[:,10:].sum().nlargest(7)
+
     ur3=st.button("click                                    me")
     if ur3:
-        plt.bar(labels, values)
+        plt.bar(y.index, y.values)
         plt.xlabel('Courses ')  # Add x-axis label
         plt.ylabel('Count ')  # Add y-axis label
         plt.title('Course Enrollment ')  # Add title
